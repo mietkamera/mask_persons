d15481283abae58c0d8810153950e745504dd979
root <root@v2202204165699187879.bestsrv.de>
2022-04-21 13:49:23 +0200

    New DEV-ENV

---
+++ mp2.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/env/bin/python3
+
+from tensorflow import keras
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+from patchify import patchify, unpatchify
+
+
+
+import pathlib
+
+model_file = str(pathlib.Path(__file__).parent.absolute()) + '/model/mk_default_model.h5'
+
+# import Model
+
+
+model = keras.models.load_model(model_file, compile=False)
+
+
